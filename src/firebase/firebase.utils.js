9d50ef9c24acff7654b877542fd19fadeccafc1a
Alec Jordan <32460719+TheDeterminator@users.noreply.github.com>
2021-05-06 11:27:43 -0400

    add redux thunk and refactor shop page

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -50,7 +50,7 @@ export const addCollectionAndDocuments = async (
   return await batch.commit();
 };
 
-export const convertCollectionsSnapshotTomap = (collections) => {
+export const convertCollectionsSnapshotToMap = (collections) => {
   const transformedCollection = collections.docs.map((doc) => {
     const { title, items } = doc.data();
 
